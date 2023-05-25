0af643cc230bd8244b979d73561bb5e4181fcdac
Leia Ang <22011199@myrp.edu.sg>
2023-05-25 15:09:24 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id22011199/mylocalbanks/MainActivity.java
@@ -0,0 +1,142 @@
+package sg.edu.rp.c346.id22011199.mylocalbanks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvDBS;
+    TextView tvOCBC;
+    TextView tvUOB;
+
+    String wordClicked = "";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvDBS = findViewById(R.id.textViewDBS);
+        tvOCBC = findViewById(R.id.textViewOCBC);
+        tvUOB = findViewById(R.id.textViewUOB);
+
+        registerForContextMenu(tvDBS);
+        registerForContextMenu(tvOCBC);
+        registerForContextMenu(tvUOB);
+
+
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+
+        //item id is 0 (Second parameter)
+        menu.add(0, 0, 0, "Website");
+
+        //item id is 1 (Second parameter)
+        menu.add(0, 1, 1, "Contact Bank");
+
+        if (v == tvDBS) {
+            wordClicked = "DBS";
+        } else if (v == tvOCBC) {
+            wordClicked = "OCBC";
+        } else if (v == tvUOB) {
+            wordClicked = "UOB";
+        }
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.EnglishSelection) {
+            tvDBS.setText("DBS");
+            tvOCBC.setText("OCBC");
+            tvOCBC.setText("UOB");
+
+            return true;
+
+
+        } else if (id == R.id.ChineseSelection) {
+            tvDBS.setText("星展银行");
+            tvOCBC.setText("华侨银行");
+            tvUOB.setText("大华银行");
+
+            return true;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        if (wordClicked.equalsIgnoreCase("DBS")) {
+            if (item.getItemId() == 0) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.dbs.com.sg"));
+                startActivity(intent);
+                return true;
+            } else if (item.getItemId() == 1) {
+                Intent intentcall = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + 1800111111));
+                startActivity(intentcall);
+                return true;
+            }
+        } else if (wordClicked.equalsIgnoreCase("OCBC")) {
+            if (item.getItemId() == 0) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.ocbc.com"));
+                startActivity(intent);
+                return true;
+            } else if (item.getItemId() == 1) {
+                Intent intentcall = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + 1800363333));
+                startActivity(intentcall);
+                return true;
+            }
+        } else if (wordClicked.equalsIgnoreCase("UOB")) {
+            if (item.getItemId() == 0) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.uob.com.sg"));
+                startActivity(intent);
+                return true;
+            } else if (item.getItemId() == 1) {
+                Intent intentcall = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + 1800222212));
+                startActivity(intentcall);
+                return true;
+            }
+        }
+        return super.onContextItemSelected(item);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
